6f876c72e102e31c0412a816c73a4f578623c2b5
GitHub Action <action@github.com>
2023-02-08 05:08:27 +0000

    Template cleanup

--- src/main/kotlin/com/github/dzqann/testideaplugin/MyBundle.kt
+++ src/main/kotlin/com/github/dzqann/testideaplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.dzqann.testideaplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/dzqann/testideaplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/dzqann/testideaplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.dzqann.testideaplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.dzqann.testideaplugin.MyBundle
 
 class MyApplicationService {
 
